22f54798b62c3390e254a4aac95715820536eb64
simone-sampino <s.samp98@gmail.com>
2025-06-17 18:50:11 +0200

    Refactor HTML structure and implement email fetching in a loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
+// DOM
+const ulEl = document.getElementById("email-list");
+
 const endPointUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-fetch(endPointUrl)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+// fetch con ciclo for per creare 10 email
+for (let i = 0; i < 10; i++) {
+  fetch(endPointUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const liEl = document.createElement("li");
+      console.log(liEl);
+
+      liEl.innerText = data.response;
+      ulEl.appendChild(liEl);
+    });
+}
